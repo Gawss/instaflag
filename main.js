01bed7ad07bfa869d26a63f208ae5e07f9d73af4
Luis M. Palacio <luispalacio1997@hotmail.com>
2022-06-17 13:14:57 +0200

    InstaFlag - Mobile

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+let SERVER_PORT = 1337;
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(process.env.PORT || SERVER_PORT, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
+    console.log('Server On', __dirname)
+});
+
+app.use(express.static(__dirname + '/Unity/Build'));
+app.use(express.static(__dirname + '/Unity'));
+app.get('/', (req, res) => {    
+    console.log('GET /');
+    res.sendFile(__dirname + '/Unity/index.html');
+});
