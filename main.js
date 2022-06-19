b85e656b17790b8ab3875e0120f6e52d1aa00dcb
Luis M. Palacio <luispalacio1997@hotmail.com>
2022-06-19 18:49:51 +0200

    PopUps Messages

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+
 let SERVER_PORT = 1337;
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +12,13 @@ const server = app.listen(process.env.PORT || SERVER_PORT, () => {
     console.log('Server On', __dirname)
 });
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.static(__dirname + '/Unity/Build'));
 app.use(express.static(__dirname + '/Unity'));
 app.get('/', (req, res) => {    
